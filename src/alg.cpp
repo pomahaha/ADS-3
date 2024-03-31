2a416831f353923e5996e419a065336c5be7919a
pomahaha <110399575+pomahaha@users.noreply.github.com>
2024-03-31 17:41:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int* arr, int size, int value) {
-	int rg = size - 1;
-	int scht = 0;
-	int lf = 0;
-	while (lf <= rg) {
-		int mdl = lf + (rg - lf) / 2;
-		if (arr[mdl] == value) {
+int rg = size - 1;
+int scht = 0;
+int lf = 0;
+while (lf <= rg) {
+	int mdl = lf + (rg - lf) / 2;
+	if (arr[mdl] == value) {
+		scht++;
+		int i = mdl + 1;
+		while (i < size && arr[i] == value) {
 			scht++;
-			int i = mdl + 1;
-			while (i < size && arr[i] == value) {
-				scht++;
-				i++;
-			}
-			i = mdl - 1;
-			while (i >= 0 && arr[i] == value) {
-				scht++;
-				i--;
-			}
-			break;
+			i++;
 		}
-		if (arr[mdl] > value)
-			rg = mdl - 1;
-		if (arr[mdl] < value)
-			lf = mdl + 1;
+		i = mdl - 1;
+		while (i >= 0 && arr[i] == value) {
+			scht++;
+			i--;
+		}
+		break;
 	}
-	return scht;
+	if (arr[mdl] > value)
+		rg = mdl - 1;
+	if (arr[mdl] < value)
+		lf = mdl + 1;
+}
+return scht;
 }
